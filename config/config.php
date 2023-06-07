9b82f7b16d67dcca3d8482cf01c52f6e1ab0401e
ASUASU01 <126759586+ASUASU01@users.noreply.github.com>
2023-06-07 18:52:02 +0300

    DONE

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ session_start();
 
 $timezone = date_default_timezone_set("Europe/Bucharest");
 
-$con = mysqli_connect("localhost", "root", "", "social"); //Connection variable
+$con = mysqli_connect("eu-cdbr-west-03.cleardb.net", "b483c78de1e73d", "", "heroku_a6ea7f8c2af936f"); //Connection variable
 
 if(mysqli_connect_errno()) 
 {
@@ -14,10 +14,10 @@ if(mysqli_connect_errno())
 
 
 // partea de resetare a parolei
-$server = "localhost";
-$username = "root";
-$password = "";
-$database = "social";
+$server = "eu-cdbr-west-03.cleardb.net";
+$username = "b483c78de1e73d";
+$password = "919e1f82";
+$database = "heroku_a6ea7f8c2af936f";
 $connection = mysqli_connect("$server","$username","$password");
 $select_db = mysqli_select_db($connection, $database);
 if(!$select_db)
