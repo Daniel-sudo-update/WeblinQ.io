2b3fd38fd9094f619cb817079c44d3c411f91a52
ASUASU01 <126759586+ASUASU01@users.noreply.github.com>
2023-06-08 11:30:45 +0300

    DONE

--- log/contact.php
+++ log/contact.php
@@ -57,12 +57,12 @@ $mail->addAttachment($target_file);
 
 // Send the message
 if (!$mail->send()) {
-    echo 'Eroare la Mailer: ' . $mail->ErrorInfo;
+    // echo 'Eroare la Mailer: ' . $mail->ErrorInfo;
 } else {
     echo '<b>Mesaj trimis, așteaptă raspunsul cu link-ul de inregistrare pe email!</b>';
 }
 
-     exit; // prevent form from displaying again
+     //exit; // prevent form from displaying again
 }
 ?>
 
