8297e6e6e8e55eaaebee273b27d80793939d2371
ASUASU01 <126759586+ASUASU01@users.noreply.github.com>
2023-06-08 09:45:05 +0300

    DONE

--- log/contact.php
+++ log/contact.php
@@ -31,7 +31,38 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // Create a new PHPMailer instance
-   
+    $mail = new PHPMailer;
+    $mail->isSMTP();
+    $mail->Host = 'smtp.mailgun.org';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'postmaster@sandbox334b587771084448b5262bfe125f2fa1.mailgun.org';
+    $mail->Password = '4b0a57462c9572aebb42aeb9177de227-6d1c649a-cd9c77ca'; // actualizează cheia de acces aici
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+    // $mail->SMTPDebug = 3;  //pentru debugging
+    // Set who the message is to be sent from
+    $mail->setFrom('confirm.informatii@gmail.com', 'Registration Form');
+    // Set an alternative reply-to address
+    $mail->addReplyTo('confirm.informatii@gmail.com', 'Registration Form');
+    // Set who the message is to be sent to
+    $mail->addAddress('confirm.informatii@gmail.com', 'Contact Info');
+
+    // Set the subject line
+    $mail->Subject = 'New Registration';
+
+    // Set the body
+    $mail->Body = "First Name: $firstName\nEmail: $email";
+
+    // Attach the uploaded file
+    $mail->addAttachment($target_file);
+
+    // Send the message
+    if (!$mail->send()) {
+        echo 'Eroare la Mailer: ' . $mail->ErrorInfo;
+    } else {
+        echo '<b>Mesaj trimis, așteaptă raspunsul cu link-ul de inregistrare pe email!</b>';
+    }
+
 
 // Instantiate the client
 $mg = Mailgun::create('4b0a57462c9572aebb42aeb9177de227-6d1c649a-cd9c77ca', 'postmaster@sandbox334b587771084448b5262bfe125f2fa1.mailgun.org');
