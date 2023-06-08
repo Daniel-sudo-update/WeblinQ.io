5d96d24c0a61b5371b903b3f8fcb3b6f788dd0c8
ASUASU01 <126759586+ASUASU01@users.noreply.github.com>
2023-06-08 10:56:49 +0300

    DONE

--- log/contact.php
+++ log/contact.php
@@ -47,6 +47,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     } catch (Exception $e) {
         echo 'Eroare la Mailer: ' . $e->getMessage();
     }
+    ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 }
 ?>
 
