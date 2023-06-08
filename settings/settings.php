9812a8a8d62666651cf522f758dbb79f40c0b493
ASUASU01 <126759586+ASUASU01@users.noreply.github.com>
2023-06-08 17:01:55 +0300

    DONE

--- settings/settings.php
+++ settings/settings.php
@@ -57,7 +57,7 @@ include("../includes/settings_handler.php");
                     <input type="file" name="profile_pic">
                 </div>
                 <div class="text-gray-700 dark:text-gray-200">
-                    <button type="submit" name="submit">Upload</button>
+                    <button type="submit" name="submit" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Upload</button>
                 </div>
             </form>
             <!-- final actualizare poza de profil -->
@@ -97,7 +97,7 @@ include("../includes/settings_handler.php");
             </div>
                 <p><?php echo $job_message; ?></p>
                 <div class="flex justify-end mt-6">
-                    <button type="submit" name="update_detailss" id="save_detailss" class="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">Save</button>
+                    <button type="submit" name="update_detailss" id="save_detailss" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Save</button>
                 </div>
         </form>
 
