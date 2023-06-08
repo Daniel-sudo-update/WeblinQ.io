21aa312ae1ab0a65e70aad6df47dce85534e64fa
ASUASU01 <126759586+ASUASU01@users.noreply.github.com>
2023-06-08 09:28:16 +0300

    DONE

--- log/contact.php
+++ log/contact.php
@@ -5,6 +5,7 @@ require("../config/config.php");
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+use Mailgun\Mailgun;
 
 require '../vendor/autoload.php';
 
@@ -30,37 +31,31 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // Create a new PHPMailer instance
-    $mail = new PHPMailer;
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'confirm.informatii@gmail.com';
-    $mail->Password = 'tpdyfxgicwupvlyk'; // actualizează cheia de acces aici
-    $mail->SMTPSecure = 'tls';
-    $mail->Port = 587;
-    // $mail->SMTPDebug = 3;  //pentru debugging
-    // Set who the message is to be sent from
-    $mail->setFrom('confirm.informatii@gmail.com', 'Registration Form');
-    // Set an alternative reply-to address
-    $mail->addReplyTo('confirm.informatii@gmail.com', 'Registration Form');
-    // Set who the message is to be sent to
-    $mail->addAddress('confirm.informatii@gmail.com', 'Contact Info');
+   
 
-    // Set the subject line
-    $mail->Subject = 'New Registration';
+// Instantiate the client
+$mg = Mailgun::create('4b0a57462c9572aebb42aeb9177de227-6d1c649a-cd9c77ca', 'postmaster@sandbox334b587771084448b5262bfe125f2fa1.mailgun.org');
 
-    // Set the body
-    $mail->Body = "First Name: $firstName\nEmail: $email";
+// Make the call to the client
+$result = $mg->messages()->send('sandbox334b587771084448b5262bfe125f2fa1.mailgun.org', [
+  'from'    => 'Registration Form <confirm.informatii@gmail.com>',
+  'to'      => 'Contact Info <confirm.informatii@gmail.com>',
+  'subject' => 'New Registration',
+  'text'    => "First Name: $firstName\nEmail: $email",
+  'attachment' => [$target_file]
+]);
 
-    // Attach the uploaded file
-    $mail->addAttachment($target_file);
+if ($result) {
+  echo '<b>Mesaj trimis, așteaptă raspunsul cu link-ul de inregistrare pe email!</b>';
+} else {
+  echo 'Eroare la Mailer: ' . $result->getMessage();
+}
 
-    // Send the message
-    if (!$mail->send()) {
-        echo 'Eroare la Mailer: ' . $mail->ErrorInfo;
-    } else {
-        echo '<b>Mesaj trimis, așteaptă raspunsul cu link-ul de inregistrare pe email!</b>';
-    }
+
+
+
+
+
     exit; // prevent form from displaying again
 }
 
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInitc4fd96c9341b6a8594441a73a6235765
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitc4fd96c9341b6a8594441a73a6235765::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
