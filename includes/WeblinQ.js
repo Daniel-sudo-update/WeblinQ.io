1f96d86aa506dca161380dabeacb613eca3ba462
ASUASU01 <126759586+ASUASU01@users.noreply.github.com>
2023-06-18 21:52:55 +0300

    DONE

--- includes/WeblinQ.js
+++ includes/WeblinQ.js
@@ -22,9 +22,9 @@ $(document).ready(function () {
                 $("#post_form").addClass("hidden"); // Ascunde modalul adăugând clasa "hidden"
                 window.location.reload(); // Reîmprospătează pagina
             },
-            error: function () {
-                alert("Failure");
-            },
+            error: function(jqXHR, textStatus, errorThrown) {
+				console.error(jqXHR, textStatus, errorThrown);
+			},
         });
     });
 });
