1f71dbbc3f6145922095a377970339b1888619b7
ASUASU01 <126759586+ASUASU01@users.noreply.github.com>
2023-06-19 12:36:26 +0300

    DONE

--- includes/ajax_submit_profile_post.php
+++ includes/ajax_submit_profile_post.php
@@ -4,6 +4,11 @@ include("../includes/user.php");
 include("../includes/Post.php");
 include("../includes/Notification.php");
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 if(isset($_POST['post_body'])) {
     $post_body = $_POST['post_body'];
 
