16ccecace31f97fb0d0be24e5802cce5d49efeff
ASUASU01 <126759586+ASUASU01@users.noreply.github.com>
2023-06-19 17:50:38 +0300

    DONE

--- includes/ajax_submit_profile_post.php
+++ includes/ajax_submit_profile_post.php
@@ -4,31 +4,31 @@ include("../includes/user.php");
 include("../includes/Post.php");
 include("../includes/Notification.php");
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 
 if(isset($_POST['post_body'])) {
     $post_body = $_POST['post_body'];
 
-    // Listează cuvintele pe care nu le permiți
-    $cuvinte_improprii = ["curva", "curvă", "muie", "muist", "muistă", "futu-i",
-						  "găoază", "găozar", "gaoaza", "gaozar", "muista", "poponar",
-						  "pulă", "pula", "pula bleaga", "pulă bleagă", "sloboz",
-						  "pupincurist", "căcănar", "cacanar", "puleasca", "puleașcă",
-						  "prost", "prostănac", "proastă", "curvar", "curist", "curista",
-						  "sugaci", "pupincurist", "labagiu", "labajiu",];
+    // // Listează cuvintele pe care nu le permiți
+    // $cuvinte_improprii = ["curva", "curvă", "muie", "muist", "muistă", "futu-i",
+	// 					  "găoază", "găozar", "gaoaza", "gaozar", "muista", "poponar",
+	// 					  "pulă", "pula", "pula bleaga", "pulă bleagă", "sloboz",
+	// 					  "pupincurist", "căcănar", "cacanar", "puleasca", "puleașcă",
+	// 					  "prost", "prostănac", "proastă", "curvar", "curist", "curista",
+	// 					  "sugaci", "pupincurist", "labagiu", "labajiu",];
     
-    // Verifică dacă postarea conține cuvinte improprii
-    foreach($cuvinte_improprii as $cuvant) {
-        if (mb_stripos($post_body, $cuvant) !== false) {
-            // În cazul în care postarea conține un cuvânt nepotrivit, oprește procesarea și trimite un mesaj de eroare
-            http_response_code(400);
-            echo "Postarea conține conținut nepotrivit.";
-            exit();
-        }
-    }
+    // // Verifică dacă postarea conține cuvinte improprii
+    // foreach($cuvinte_improprii as $cuvant) {
+    //     if (mb_stripos($post_body, $cuvant) !== false) {
+    //         // În cazul în care postarea conține un cuvânt nepotrivit, oprește procesarea și trimite un mesaj de eroare
+    //         http_response_code(400);
+    //         echo "Postarea conține conținut nepotrivit.";
+    //         exit();
+    //     }
+    // }
 
     $apiKey = getenv('AZURE_API_KEY'); // cheia API a resursei Content Moderator
     $endpoint = "https://weblinqmoderator.cognitiveservices.azure.com/contentmoderator/moderate/v1.0/ProcessText/Screen?classify=True"; // punctul de acces al resursei Content Moderator
