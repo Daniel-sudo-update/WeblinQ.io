9c5b268cc8c9e97a4df8dc1c80a1bdfa6c98b9a2
ASUASU01 <126759586+ASUASU01@users.noreply.github.com>
2023-06-22 20:01:51 +0300

    DONE

--- includes/header.php
+++ includes/header.php
@@ -299,9 +299,23 @@ if (isset($_SESSION['username'])) {
           <p  class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"><?php echo "Postari: " . $user['num_posts']; ?></p>
         
           <p  class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"><?php echo "Aprecieri: " . $user['num_likes']; ?></p>
-        
-          <p  class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"><?php echo "Prieteni: " . $user['friend_array']; ?><br></p>
+        <!-- prieteni -->
+          <!-- <p  class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"><?php echo "Prieteni: " . $user['friend_array']; ?><br></p> -->
           
+        </li>
+
+        <li>
+        <p  class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"> 
+          <button onclick="redirect()" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                   Mesagerie
+          </button>
+
+                <script>
+                    function redirect() {
+                        window.location.href = "../templates/messages.php";
+                    }
+                </script>
+        </p>
         </li>
 
          <li>
